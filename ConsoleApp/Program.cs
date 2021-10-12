0fc2e93b281bbf8e69acbd42729943a614a64b3c
Natali <Natali@DESKTOP-CRPU105>
2021-10-12 16:13:34 +0300

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyFirstConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine(a);
+           // Console.ReadKey();
+
+        }
+    }
+}
---
+++ MySecondConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MySecondConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            decimal pi = 3.141592653M;
+            decimal e = 1.414213562373095048M;
+
+
+            Console.WriteLine(pi);
+            Console.WriteLine(e);
+            
+
+        }
+    }
+}
